a4e1ac550ccb8ea147e57bff17db91e63bc1f6be
yu-m-0212 <ui.others@gmail.com>
2018-08-11 14:24:52 +0900

    18/08/11 マップ変更

---
+++ data/Map/tmx/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MapSetting_org" tilewidth="32" tileheight="32" tilecount="114" columns="19">
+ <image source="../../../../../Downloads/TiledMapEditer説明書/MapSetting_org.png" width="609" height="193"/>
+</tileset>
